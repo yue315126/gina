38260e2b189890362ed75e68acf89a24ebdc260a
yue315126 <s1092186@gm.pu.edu.tw>
2022-06-05 16:35:11 +0800

    gian 第三次commit

--- app/src/main/java/com/example/gina/MainActivity.kt
+++ app/src/main/java/com/example/gina/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.gina
 
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.GenericTransitionOptions.with
+import com.bumptech.glide.Glide.with
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.with
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.with
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var img : ImageView
-    lateinit var game : Game
-    var flag:Boolean = false
-    lateinit var job : Job
+    lateinit var img: ImageView
+    lateinit var game: Game
+    var flag: Boolean = false
+    lateinit var job: Job
+    lateinit var imgAuthor: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +31,24 @@ class MainActivity : AppCompatActivity() {
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
 
+        imgAuthor = findViewById(R.id.imgAuthor)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.gina)
+            .circleCrop()
+            .override(800, 600)
+            .into(imgAuthor)
+
         img.setOnClickListener({
-            if (flag){
+            if (flag) {
                 flag = false
                 img.setImageResource(R.drawable.start)
                 job.cancel()
-            }
-            else{
+            } else {
                 flag = true
                 img.setImageResource(R.drawable.stop)
                 job = GlobalScope.launch(Dispatchers.Main) {
-                    while(flag) {
+                    while (flag) {
                         delay(10)
                         var canvas: Canvas = game.surfaceHolder.lockCanvas()
                         game.drawSomething(canvas)
---
+++ app/src/test/java/com/example/gina/GlideApp.kt
@@ -0,0 +1,5 @@
+package com.example.gina
+
+class GlideApp {
+
+}
