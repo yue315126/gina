91984da81d12e21d5ac36eefc6b2c18c482eb71d
yue315126 <s1092186@gm.pu.edu.tw>
2022-06-05 15:35:04 +0800

    gian 第一次commit

---
+++ app/src/main/java/com/example/gina/Game.kt
@@ -0,0 +1,50 @@
+package com.example.gina
+
+import android.content.Context
+import android.graphics.*
+import android.media.MediaPlayer
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class Game(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+SurfaceHolder.Callback {
+
+    var surfaceHolder: SurfaceHolder
+    var BG: Bitmap
+
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
+        surfaceHolder.addCallback(this)
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+        drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+
+    }
+
+    fun drawSomething(canvas: Canvas) {
+        var SrcRect: Rect = Rect(0, 0, BG.width, BG.height) //裁切
+        var w:Int = width
+        var h:Int = height
+        var DestRect: Rect = Rect(0, 0, w, h)
+        canvas.drawBitmap(BG, SrcRect, DestRect, null)
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("射擊遊戲(作者：陳芷柔)",50f,50f, paint)
+    }
+
+}
