0e957e2731aaa0ef7f0108678d4da72f68d86aa0
yue315126 <s1092186@gm.pu.edu.tw>
2022-06-05 16:02:32 +0800

    gian 第二次commit

--- app/src/main/java/com/example/gina/Game.kt
+++ app/src/main/java/com/example/gina/Game.kt
@@ -13,7 +13,7 @@ SurfaceHolder.Callback {
 
     var surfaceHolder: SurfaceHolder
     var BG: Bitmap
-
+    var BGmoveX:Int = 0
     init {
         surfaceHolder = getHolder()
         BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
@@ -40,6 +40,22 @@ SurfaceHolder.Callback {
         var h:Int = height
         var DestRect: Rect = Rect(0, 0, w, h)
         canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        //canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        BGmoveX -= 2
+        var BGnewX:Int = w + BGmoveX
+
+        // 如果已捲動整張圖，則重新開始
+        if (BGnewX <= 0) {
+            BGmoveX = 0
+            // only need one draw
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        } else {
+            // need to draw original and wrap
+            DestRect = Rect(BGmoveX, 0, BGmoveX+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+            DestRect = Rect(BGnewX, 0, BGnewX+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        }
 
         val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.color = Color.BLUE
--- app/src/main/java/com/example/gina/MainActivity.kt
+++ app/src/main/java/com/example/gina/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.gina
 
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img : ImageView
+    lateinit var game : Game
+    var flag:Boolean = false
+    lateinit var job : Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        img = findViewById(R.id.img)
+        game = findViewById(R.id.game)
+
+        img.setOnClickListener({
+            if (flag){
+                flag = false
+                img.setImageResource(R.drawable.start)
+                job.cancel()
+            }
+            else{
+                flag = true
+                img.setImageResource(R.drawable.stop)
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(flag) {
+                        delay(10)
+                        var canvas: Canvas = game.surfaceHolder.lockCanvas()
+                        game.drawSomething(canvas)
+                        game.surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+
+        })
     }
 }
