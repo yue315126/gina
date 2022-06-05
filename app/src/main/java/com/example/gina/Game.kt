3a02d2680d56f0816fe21e3398e46eff3bb9965f
yue315126 <s1092186@gm.pu.edu.tw>
2022-06-05 16:54:43 +0800

    gian 第四次commit

--- app/src/main/java/com/example/gina/Game.kt
+++ app/src/main/java/com/example/gina/Game.kt
@@ -5,19 +5,25 @@ import android.graphics.*
 import android.media.MediaPlayer
 import android.util.AttributeSet
 import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class Game(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
-SurfaceHolder.Callback {
+SurfaceHolder.Callback, GestureDetector.OnGestureListener {
 
     var surfaceHolder: SurfaceHolder
     var BG: Bitmap
-    var BGmoveX:Int = 0
+    var BGmoveX: Int = 0
+    var fly: Fly
+    var gDetector: GestureDetector
+
     init {
         surfaceHolder = getHolder()
         BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
         surfaceHolder.addCallback(this)
+        fly = Fly(context!!)
+        gDetector = GestureDetector(context, this)
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
@@ -36,13 +42,12 @@ SurfaceHolder.Callback {
 
     fun drawSomething(canvas: Canvas) {
         var SrcRect: Rect = Rect(0, 0, BG.width, BG.height) //裁切
-        var w:Int = width
-        var h:Int = height
+        var w: Int = width
+        var h: Int = height
         var DestRect: Rect = Rect(0, 0, w, h)
-        canvas.drawBitmap(BG, SrcRect, DestRect, null)
         //canvas.drawBitmap(BG, SrcRect, DestRect, null)
         BGmoveX -= 2
-        var BGnewX:Int = w + BGmoveX
+        var BGnewX: Int = w + BGmoveX
 
         // 如果已捲動整張圖，則重新開始
         if (BGnewX <= 0) {
@@ -51,16 +56,47 @@ SurfaceHolder.Callback {
             canvas.drawBitmap(BG, SrcRect, DestRect, null)
         } else {
             // need to draw original and wrap
-            DestRect = Rect(BGmoveX, 0, BGmoveX+w, h)
+            DestRect = Rect(BGmoveX, 0, BGmoveX + w, h)
             canvas.drawBitmap(BG, SrcRect, DestRect, null)
-            DestRect = Rect(BGnewX, 0, BGnewX+w, h)
+            DestRect = Rect(BGnewX, 0, BGnewX + w, h)
             canvas.drawBitmap(BG, SrcRect, DestRect, null)
         }
 
         val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.color = Color.BLUE
         paint.textSize = 50f
-        canvas.drawText("射擊遊戲(作者：陳芷柔)",50f,50f, paint)
+        canvas.drawText("射擊遊戲(作者：陳芷柔)", 50f, 50f, paint)
+
+        fly.draw(canvas)
     }
 
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, p2: Float, p3: Float): Boolean {
+        fly.y = e2!!.y.toInt() - fly.h / 2
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        gDetector.onTouchEvent(event)
+        return true
+    }
 }
--- app/src/main/java/com/example/gina/MainActivity.kt
+++ app/src/main/java/com/example/gina/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.gina
 
 import android.graphics.Canvas
-import android.icu.number.NumberFormatter.with
-import android.icu.number.NumberRangeFormatter.with
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import com.bumptech.glide.GenericTransitionOptions.with
-import com.bumptech.glide.Glide.with
 import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.with
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.with
 import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
 @GlideModule
@@ -50,6 +44,8 @@ class MainActivity : AppCompatActivity() {
                 job = GlobalScope.launch(Dispatchers.Main) {
                     while (flag) {
                         delay(10)
+
+                        game.fly.update()
                         var canvas: Canvas = game.surfaceHolder.lockCanvas()
                         game.drawSomething(canvas)
                         game.surfaceHolder.unlockCanvasAndPost(canvas)
